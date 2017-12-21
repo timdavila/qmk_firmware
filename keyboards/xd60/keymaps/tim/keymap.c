eace15ddd3a27866c6150278d474f492ec5c8822
Tim Davila <tim.davila@pursuant.com>
2017-12-21 16:22:02 -0600

    add word delete on caps+;

--- keyboards/xd60/keymaps/tim/keymap.c
+++ keyboards/xd60/keymaps/tim/keymap.c
@@ -15,16 +15,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
       RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_PAUS,  KC_PSCR,   \
       KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,RGB_VAI,RGB_HUD,RGB_SAD,RGB_VAD,KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_DEL,    \
-      KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL, KC_END, KC_PGDN,KC_NO,  KC_NO,  KC_NO,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_NO,   KC_APP,  BL_STEP,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_NO,   KC_PGUP,  F(0),      \
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_APP,  KC_MUTE,  KC_VOLD, KC_VOLU,  KC_END),
+      KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_NO,   KC_NO,   BL_STEP,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    F(0),      \
+      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_APP,  KC_NO,    KC_NO,   KC_NO,    KC_END),
 
   // 2: Extend Layer
   KEYMAP(
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_MUTE, KC_VOLD,  KC_VOLU, KC_PAUS,  KC_PSCR,   \
-      KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,RGB_VAI,RGB_HUD,RGB_SAD,RGB_VAD,KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_DEL,    \
-      F(1),    KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,KC_NO,   KC_NO,    KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_NO,   KC_APP,  BL_STEP,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,     \
+      KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,RGB_VAI,RGB_HUD,RGB_SAD,RGB_VAD,KC_NO,   KC_NO,   KC_SLCK,  KC_PAUS,           KC_DEL,    \
+      F(1),    KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,LCTL(KC_BSPC),KC_NO,KC_NO,            KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,     \
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_BSPC,                         KC_RGUI, KC_HOME,  KC_PGDN, KC_PGUP,  KC_END),
 
   // RGB Control
