eee4f1645d64687d102ae9167686d18b6400b769
Tim Davila <tim.davila@pursuant.com>
2018-03-08 11:52:31 -0600

    use standard backslash, and backspace, add grave escape feature and extend+esc for grave

--- keyboards/xd60/config.h
+++ keyboards/xd60/config.h
@@ -75,5 +75,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define IS_COMMAND() ( \
   keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
+/* grave esc fix for key combinations using esc */
+#define GRAVE_ESC_ALT_OVERRIDE
+#define GRAVE_ESC_GUI_OVERRIDE
 
 #endif
