1e6602af9992d1215c0131ebc32261d85b260bc1
Tim Davila <tim.davila@pursuant.com>
2018-08-21 13:32:53 -0500

    updated keymap for windows layout

--- keyboards/xd60/keymaps/tim/keymap.c
+++ keyboards/xd60/keymaps/tim/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,            KC_BSPC,   \
       F(1),    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,              KC_ENT,    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT,   F(0),      \
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RALT, KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT),
+      KC_LGUI, KC_LALT, KC_LCTL,                          KC_SPC,                          KC_RCTL, KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT),
 
   // 1: Function Layer
   KEYMAP(
@@ -17,15 +17,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,RGB_VAI,RGB_HUD,RGB_SAD,RGB_VAD,KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_NO,    \
       KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_MPRV,KC_MPLY,KC_NO,  KC_MNXT, KC_NO,   KC_NO,    KC_NO,             KC_ENT,    \
       KC_LSFT, KC_NO,   KC_NO,   KC_NO,   BL_STEP,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    F(0),      \
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_APP,  KC_NO,    KC_NO,   KC_NO,    KC_END),
+      KC_LGUI, KC_LALT, KC_LCTL,                          KC_SPC,                          KC_APP,  KC_NO,    KC_NO,   KC_NO,    KC_END),
 
   // 2: Extend Layer
   KEYMAP(
-      KC_GRV,  KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_MUTE, KC_VOLD,  KC_VOLU, KC_NO,    KC_PSCR,   \
-      KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,RGB_VAI,RGB_HUD,RGB_SAD,RGB_VAD,KC_NO,   KC_NO,   KC_SLCK,  KC_PAUS,           KC_DEL,    \
-      F(1),    KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,KC_NO,   KC_NO,    KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,     \
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_BSPC,                         KC_RGUI, KC_HOME,  KC_PGDN, KC_PGUP,  KC_END),
+      KC_GRV,  KC_NO,     KC_NO,     KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_MUTE, KC_VOLD,  KC_VOLU, KC_NO,    KC_PSCR,   \
+      KC_NO,   KC_NO,     KC_NO,     KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_SLCK,  KC_PAUS,           KC_DEL,    \
+      F(1),    LGUI(KC_A),LGUI(KC_S),KC_NO,   KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,KC_NO,   KC_NO,    KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,LGUI(KC_Z),LGUI(KC_X),LGUI(KC_C),KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,     \
+      KC_LGUI, KC_LALT,   KC_LCTL,                          KC_BSPC,                         KC_RGUI, KC_HOME,  KC_PGDN, KC_PGUP,  KC_END),
 
   // RGB Control
   //  Q: ON/OFF
